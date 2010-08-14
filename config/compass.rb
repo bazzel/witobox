d9dd35c54ddd9023cc4595f9a9786429d76e637d
Patrick Baselier <patrick.baselier@gmail.com>
2010-08-14 12:05:34 +0200

    Another sass-heroku attempt.

--- config/compass.rb
+++ config/compass.rb
@@ -1,27 +1,14 @@
-Rails::Configuration.class_eval do
-
-  attr_writer :heroku
-
-  def initialize   
-    super
-    @heroku = !!ENV['HEROKU_TYPE']
-  end
-
-  def heroku?
-    @heroku   
-  end
-
-end
-
 # This configuration file works with both the Compass command line tool and within Rails.
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
+# Do not set css_dir, let Hassle take care of it and get compiled css files from public/stylesheets.
 # css_dir = "public/stylesheets/compiled"
-css_dir = defined?(Rails) && Rails.configuration.heroku? ? "tmp/stylesheets/compiled" : "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+# Specify template_location to make Hassle look in the sass_dir.
+Sass::Plugin.options[:template_location] = sass_dir
