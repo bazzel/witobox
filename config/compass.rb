0c883be0e7dc370ebdb4eab531ee3850081204b8
Patrick Baselier <patrick.baselier@gmail.com>
2010-08-14 13:01:07 +0200

    Use compiled css instead of hassle, too bad.

--- config/compass.rb
+++ config/compass.rb
@@ -4,11 +4,8 @@ project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
-# Do not set css_dir, let Hassle take care of it and get compiled css files from public/stylesheets.
-# css_dir = "public/stylesheets/compiled"
+css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-# Specify template_location to make Hassle look in the sass_dir.
-Sass::Plugin.options[:template_location] = sass_dir
